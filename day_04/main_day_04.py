cc504a2bfa07aee43b3384b6092f161246eecd66
Wolfram Friele <wolframfriele@gmail.com>
2020-12-04 22:25:33 +0100

    Day 4 stage 2

--- day_04/main_day_04.py
+++ day_04/main_day_04.py
@@ -1,16 +1,16 @@
 # import data
-
 passport_raw_data = open('passports.txt', 'r')
 passports_split = passport_raw_data.read().split('\n\n')
 passport_raw_data.close()
 passports = []
 
-
+# split into field per password
 for passport in passports_split:
     passports.append([item.strip() for item in passport.split()])
 
 passports_clean = []
 
+# add fields to dictionary
 for passport in passports:
     passports_dict = {}
     for field in passport:
@@ -24,8 +24,43 @@ checks = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 for passport in passports_clean:
     valid = 0
     for key in passport:
-        if key in checks:
-            valid += 1
+        if key == 'byr':
+            if len(passport[key]) == 4:
+                if 2002 >= int(passport[key]) >= 1920:
+                    valid += 1
+        if key == 'iyr':
+            if len(passport[key]) == 4:
+                if 2020 >= int(passport[key]) >= 2010:
+                    valid += 1
+        if key == 'eyr':
+            if len(passport[key]) == 4:
+                if 2030 >= int(passport[key]) >= 2020:
+                    valid += 1
+        if key == 'hgt':
+            if passport[key][-2:] == 'cm':
+                if 193 >= int(passport[key][:-2]) >= 150:
+                    valid += 1
+            if passport[key][-2:] == 'in':
+                if 76 >= int(passport[key][:-2]) >= 59:
+                    valid += 1
+        if key == 'hcl':
+            if passport[key][:1] == '#':
+                if len(passport[key][1:]) == 6:
+                    accepted_char = '0123456789abcdef'
+                    accepted = True
+                    for i in passport[key][1:]:
+                        if i not in accepted_char:
+                            accepted = False
+                    if accepted:
+                        valid += 1
+        if key == 'ecl':
+            accepted_ecl = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+            if passport[key] in accepted_ecl:
+                valid += 1
+        if key == 'pid':
+            if len(passport[key]) == 9:
+                valid += 1
+
     if valid == 7:
         valid_passports += 1
 
