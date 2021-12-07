52289756e77bf11e2479492ebd7d4156a09ee4a4
Wolframfriele <wolframfriele@gmail.com>
2021-12-07 07:47:34 +0100

    Update code with sum formula

--- 2021/day_07.py
+++ 2021/day_07.py
@@ -2,29 +2,36 @@ import math
 from tools.data import ReadData
 
 ### Parse input ###
-data = ReadData("2021/data/7_t.txt", lines=False, read_int=False)
+data = ReadData("2021/data/7.txt", lines=False, read_int=False)
 data.special_split(",")
 horizontal_pos = [int(i) for i in data]
 
 # print(horizontal_pos)
 ### Solutions ###
 
-def calc_cost(pos_list, pos):
+def calc_cost(pos_list, pos, crab_engineering=False):
     total_cost = 0
     for i in pos_list:
-        total_cost += abs(i - pos)
+        if crab_engineering:
+            dist = abs(i - pos)
+            total_cost += int(dist * (dist+1) / 2)
+
+        else:
+            total_cost += abs(i - pos)
     
     return total_cost
 
-def find_cheapest(pos_list):
+def find_cheapest(pos_list, crab_engineering=False):
     cheapest_pos = 0
-    cheapest_cost = sum(pos_list)
+    cheapest_cost = 10000000000000000000000
     for pos in range(max(pos_list)+1):
-        curr_cost = calc_cost(pos_list, pos)
+        curr_cost = calc_cost(pos_list, pos, crab_engineering)
         # print(pos, curr_cost)
         if curr_cost < cheapest_cost:
             cheapest_cost = curr_cost
             cheapest_pos = pos
     return cheapest_pos, cheapest_cost
 
-print("Cheapest", find_cheapest(horizontal_pos))
+print("Cheapest", find_cheapest(horizontal_pos, crab_engineering=True))
+
+#101571302
