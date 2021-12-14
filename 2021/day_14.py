c3c77e3f400e784af02f533a6feab8798cfb3818
Wolframfriele <wolframfriele@gmail.com>
2021-12-14 22:11:35 +0100

    Day 14 Part 2

--- 2021/day_14.py
+++ 2021/day_14.py
@@ -1,39 +1,53 @@
-import re
 from tools.data import ReadData
-# NNCB
+
 ### Parse input ###
-polymer_template_read = ReadData("2021/data/14_t.txt", lines=True, read_int=False)
-polymer_template_read.special_split(' -> ')
+polymer_rules_read = ReadData("2021/data/14.txt", lines=True, read_int=False)
+polymer_rules_read.special_split(' -> ')
 
 class Polymerization(object):
-    def __init__(self, polymer_template) -> None:
-        # self.poly_template = polymer_template
-        self.poly_rules = {}
-        for rule in polymer_template:
-            pair, insert = rule
-            self.poly_rules[pair] = insert
-        self.poly = ''
+    def __init__(self, polymer_rules) -> None:
+        self.poly_rules = {pair:ins for pair, ins in polymer_rules}
+        self.pairs = {pair:0 for pair, __ in polymer_rules}
         self.elements = {}
         super().__init__()
 
-    def extend_polymer(self, input_polymer, steps):
-        self.poly = input_polymer
-        for element in input_polymer:
-            self.elements[element] = self.elements.get(element, 0) + 1
-        for idx, __ in enumerate(self.poly):
-            current_pair = self.poly[idx-1:idx+1]
-            self.split_pair(current_pair, steps)
+    def extend_polymer(self, polymer_template, steps):
+        # for element in polymer_template:
+        #     self.elements[element] = self.elements.get(element, 0) + 1
+        # print(self.pairs, '\n')
+        for idx, __ in enumerate(polymer_template):
+            pair = polymer_template[idx-1:idx+1]
+            # print(pair)
+            if pair in self.pairs:
+                self.pairs[pair] += 1
+        # print(self.pairs, '\n')
+        for __ in range(steps):
+            self.poly_extend_iteration()
+            # print(self.pairs, '\n')
+        self.update_elements()
+        self.commom_elements()
 
-    def split_pair(self, pair, ttl):
-        alive = ttl > 0
-        if pair in self.poly_rules and alive:
-            insert = self.poly_rules[pair]
-            self.elements[insert] = self.elements.get(insert, 0) + 1
-            pair1, pair2 = pair[0] + insert, insert + pair[1]
-            self.split_pair(pair1, ttl - 1)
-            self.split_pair(pair2, ttl - 1)
-            print(self.elements, ttl)
-    
+    def poly_extend_iteration(self):
+        itr_pairs = {pair:0 for pair, __ in self.poly_rules.items()}
+        for pair, amount in self.pairs.items():
+            if amount > 0:
+                insert = self.poly_rules[pair]
+                pair1, pair2 = pair[0] + insert, insert + pair[1]
+                itr_pairs[pair1] += amount
+                itr_pairs[pair2] += amount
+        for key, value in itr_pairs.items():
+            self.pairs[key] = value
+
+    def update_elements(self):
+        elements = {}
+        for pair, amount in self.pairs.items():
+            element1, element2 = pair
+            elements[element1] = elements.get(element1, 0) + amount
+            elements[element2] = elements.get(element2, 0) + amount
+        for key, value in elements.items():
+            elements[key] = value
+        self.elements = elements
+
     def commom_elements(self):
         min_value, min_key, max_value, max_key = float('inf'), '', 0, ''
         for key, value in self.elements.items():
@@ -42,10 +56,9 @@ class Polymerization(object):
             if value > max_value:
                 max_value, max_key = value, key
         print(f'Most common: {max_value, max_key}, least common: {min_value, min_key}')
-        print(f'answer = {max_value - min_value}')
+        print(f'answer = {(max_value - min_value) // 2}')
 
 # Part 1
-poly = Polymerization(polymer_template_read)
-poly.extend_polymer('NNCB', steps=10) # Test code
-# poly.extend_polymer('SHHBNFBCKNHCNOSHHVFF', steps=10) # Test code
-poly.commom_elements()
+poly = Polymerization(polymer_rules_read)
+# poly.extend_polymer('NNCB', steps=40) # Test code
+poly.extend_polymer('SHHBNFBCKNHCNOSHHVFF', steps=10)
