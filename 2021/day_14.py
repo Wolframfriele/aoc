65f0e75a8e6275c8817b3f38a402fcc108411940
Wolframfriele <wolframfriele@gmail.com>
2021-12-14 12:47:16 +0100

    Day 14 attempt 2 recursive

--- 2021/day_14.py
+++ 2021/day_14.py
@@ -2,7 +2,7 @@ import re
 from tools.data import ReadData
 # NNCB
 ### Parse input ###
-polymer_template_read = ReadData("2021/data/14.txt", lines=True, read_int=False)
+polymer_template_read = ReadData("2021/data/14_t.txt", lines=True, read_int=False)
 polymer_template_read.special_split(' -> ')
 
 class Polymerization(object):
@@ -13,38 +13,39 @@ class Polymerization(object):
             pair, insert = rule
             self.poly_rules[pair] = insert
         self.poly = ''
+        self.elements = {}
         super().__init__()
 
     def extend_polymer(self, input_polymer, steps):
         self.poly = input_polymer
-        for __ in range(steps):
-            print(__)
-            changes = []
-            for idx, __ in enumerate(self.poly):
-                current = self.poly[idx-1:idx+1]
-                if current in self.poly_rules:
-                    insert = self.poly_rules[current]
-                    changes.append((idx, insert))
-            changes.sort()
-            for i, change in enumerate(changes):
-                idx, insert = change
-                self.poly = self.poly[:idx + i] + insert + self.poly[idx + i:]
+        for element in input_polymer:
+            self.elements[element] = self.elements.get(element, 0) + 1
+        for idx, __ in enumerate(self.poly):
+            current_pair = self.poly[idx-1:idx+1]
+            self.split_pair(current_pair, steps)
+
+    def split_pair(self, pair, ttl):
+        alive = ttl > 0
+        if pair in self.poly_rules and alive:
+            insert = self.poly_rules[pair]
+            self.elements[insert] = self.elements.get(insert, 0) + 1
+            pair1, pair2 = pair[0] + insert, insert + pair[1]
+            self.split_pair(pair1, ttl - 1)
+            self.split_pair(pair2, ttl - 1)
+            print(self.elements, ttl)
     
     def commom_elements(self):
-        check_values = set(self.poly)
-        amounts = []
-        for value in check_values:
-            count = self.poly.count(value)
-            amounts.append((count, value))
-        amounts.sort()
-        most_common, least_common = amounts[-1], amounts[0]
-        print(f'Most common: {most_common}, least common: {least_common}')
-        print(f'answer = {most_common[0] - least_common[0]}')
+        min_value, min_key, max_value, max_key = float('inf'), '', 0, ''
+        for key, value in self.elements.items():
+            if value < min_value:
+                min_value, min_key = value, key
+            if value > max_value:
+                max_value, max_key = value, key
+        print(f'Most common: {max_value, max_key}, least common: {min_value, min_key}')
+        print(f'answer = {max_value - min_value}')
 
 # Part 1
 poly = Polymerization(polymer_template_read)
-# poly.extend_polymer('NNCB', steps=10) # Test code
-poly.extend_polymer('SHHBNFBCKNHCNOSHHVFF', steps=40) # Test code
+poly.extend_polymer('NNCB', steps=10) # Test code
+# poly.extend_polymer('SHHBNFBCKNHCNOSHHVFF', steps=10) # Test code
 poly.commom_elements()
-
-# Part 2
