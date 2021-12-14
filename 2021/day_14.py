f4cef2e592506f911d2931db7ef72bde91b48d94
Wolframfriele <wolframfriele@gmail.com>
2021-12-14 11:11:24 +0100

    Day 14 part 1

--- 2021/day_14.py
+++ 2021/day_14.py
@@ -2,42 +2,49 @@ import re
 from tools.data import ReadData
 # NNCB
 ### Parse input ###
-polymer_template_read = ReadData("2021/data/14_t.txt", lines=True, read_int=False)
+polymer_template_read = ReadData("2021/data/14.txt", lines=True, read_int=False)
 polymer_template_read.special_split(' -> ')
 
 class Polymerization(object):
     def __init__(self, polymer_template) -> None:
-        self.poly_template = polymer_template
+        # self.poly_template = polymer_template
+        self.poly_rules = {}
+        for rule in polymer_template:
+            pair, insert = rule
+            self.poly_rules[pair] = insert
         self.poly = ''
         super().__init__()
 
     def extend_polymer(self, input_polymer, steps):
         self.poly = input_polymer
         for __ in range(steps):
+            print(__)
             changes = []
-            for rule in self.poly_template:
-                pair, insert = rule
-                result = re.finditer(pair, self.poly, flags=0)
-                for i in result:
-                    if __ == 2 and pair == 'BB':
-                        print(i.span())
-                    index = i.span()[0] + 1
-                    if index > 0:
-                        changes.append((index, insert))
-
+            for idx, __ in enumerate(self.poly):
+                current = self.poly[idx-1:idx+1]
+                if current in self.poly_rules:
+                    insert = self.poly_rules[current]
+                    changes.append((idx, insert))
             changes.sort()
             for i, change in enumerate(changes):
-                index, insert = change
-                self.poly = self.poly[:index + i] + insert + self.poly[index + i:]
-        print(changes)
-        print('step 2: \t', 'NBCCNBBBCBHCB')
-        print('actual: \t', self.poly)
-        print('step 3: \t', 'NBBBCNCCNBBNBNBBCHBHHBCHB')
-        print(self.poly == 'NBBBCNCCNBBNBNBBCHBHHBCHB')
-
+                idx, insert = change
+                self.poly = self.poly[:idx + i] + insert + self.poly[idx + i:]
+    
+    def commom_elements(self):
+        check_values = set(self.poly)
+        amounts = []
+        for value in check_values:
+            count = self.poly.count(value)
+            amounts.append((count, value))
+        amounts.sort()
+        most_common, least_common = amounts[-1], amounts[0]
+        print(f'Most common: {most_common}, least common: {least_common}')
+        print(f'answer = {most_common[0] - least_common[0]}')
 
 # Part 1
 poly = Polymerization(polymer_template_read)
-poly.extend_polymer('NNCB', steps=3)
+# poly.extend_polymer('NNCB', steps=10) # Test code
+poly.extend_polymer('SHHBNFBCKNHCNOSHHVFF', steps=40) # Test code
+poly.commom_elements()
 
 # Part 2
