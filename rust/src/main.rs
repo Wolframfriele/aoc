b8606c656b59abc47505cd06c2a4745314abca47
Wolframfriele <wolframfriele@gmail.com>
2022-11-01 21:18:00 +0100

    Day 2 part A

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,26 @@
+use std::vec;
 
-fn part_a(input: &str) -> i32 {
-    // 2*l*w + 2*w*h + 2*h*l
+fn calculate_area(l: u32, w: u32, h: u32) -> u32 {
+    let side_a: u32 = l * w;
+    let side_b: u32 = w * h;
+    let side_c: u32 = h * l;
+
+    let smallest: u32 = *vec![side_a, side_b, side_c].iter().min().unwrap();
+
+    return 2 * side_a + 2 * side_b + 2 * side_c + smallest
+}
+
+fn part_a(input: &str) -> u32 {
+    let mut total_area: u32 = 0;
+    for line in input.trim().lines() {
+        let mut lengths = line.split("x");
+        let l = lengths.next().unwrap().parse::<u32>().unwrap();
+        let w = lengths.next().unwrap().parse::<u32>().unwrap();
+        let h = lengths.next().unwrap().parse::<u32>().unwrap();
+
+        total_area += calculate_area(l, w, h)
+    }
+    return total_area
 
 }
 
@@ -9,7 +29,7 @@ fn part_a(input: &str) -> i32 {
 // }
 
 fn main() {
-    let file = include_str!("../input/day_01_real.txt");
+    let file = include_str!("../input/day_02_real.txt");
     let answer_a = part_a(file);
     println!("Answer A: {answer_a}");
     // let answer_b = part_b(file);
