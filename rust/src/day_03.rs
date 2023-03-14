33c8bf18a1bcd9012e52ecb7c9cb15ddcfc7a1ca
Wolframfriele <wolframfriele@gmail.com>
2023-03-14 22:02:11 +0100

    feat: add wip of day 3

---
+++ rust/src/day_03.rs
@@ -0,0 +1,24 @@
+fn part_a(input: &str) -> u32 {
+    for direction in input.chars() {
+        match direction {
+            '>' => println!("Right"),
+            '<' => println!("Left"),
+            '^' => println!("Up"),
+            'v' => println!("Down"),
+            _ => (),
+        }
+    }
+    32
+}
+
+// fn part_b(input: &str) -> u32 {
+
+// }
+
+pub fn run_day() {
+    let file = include_str!("../input/day_03_real.txt");
+    let answer_a = part_a(file);
+    println!("Answer A: {answer_a}");
+    // let answer_b = part_b(file);
+    // println!("Answer B: {answer_b}");
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,34 +1,34 @@
 use clap::Parser;
 mod day_01;
 mod day_02;
+mod day_03;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-   /// The AOC day to run
-   #[arg(short, long, default_value = None)]
-   day: Option<u8>,
+    /// The AOC day to run
+    #[arg(short, long, default_value = None)]
+    day: Option<u8>,
 
-   /// If all day's need to be run
-   #[arg(short, long)]
-   all: bool,
+    /// If all day's need to be run
+    #[arg(short, long)]
+    all: bool,
 }
 
 fn main() {
-   let args = Args::parse();
+    let args = Args::parse();
 
     if args.all {
         println!("All day's will be run")
     } else {
         match args.day {
             None => println!("Run last day"),
-            Some(num) => {
-                match num {
-                    1 => day_01::run_day(),
-                    2 => day_02::run_day(),
-                    _ => println!("Day not implemented.")
-                }
-            }
+            Some(num) => match num {
+                1 => day_01::run_day(),
+                2 => day_02::run_day(),
+                3 => day_03::run_day(),
+                _ => println!("Day not implemented."),
+            },
         }
-    } 
+    }
 }
