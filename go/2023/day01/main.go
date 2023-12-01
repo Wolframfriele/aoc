1901f99d74502cb6c5dec19de0a77d031839e7c5
Wolfram Friele <wolframfriele@gmail.com>
2023-12-01 14:33:53 +0100

    2023 Day 1 part 2

--- go/2023/day01/main.go
+++ go/2023/day01/main.go
@@ -11,20 +11,41 @@ import (
 // Find the calibration digit by combining the first and last digit in the string.
 // Sum all calibration values
 
-func findDigits(line string) int {
+func getNumber(line string, i int, accept_word bool) rune {
+	if unicode.IsDigit(rune(line[i])) {
+		return rune(line[i])
+	}
+	if accept_word {
+		validNumWords := [9]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+		for numIndex, number := range validNumWords {
+			if i+len(number) <= len(line) {
+				if strings.Contains(line[i:i+len(number)], number) {
+					// return the index + 1 to match position in string slice
+					// Then add + '0' to match the string converted runes
+					return rune(numIndex+1) + '0'
+				}
+			}
+		}
+	}
+	return ' '
+}
+
+func findDigits(line string, accept_word bool) int {
 	// finds the first and last digit in a string
 	// returns them as a number
 	var digit1 rune
 	var digit2 rune
-	for _, char := range line {
-		if unicode.IsDigit(char) {
-			digit1 = char
+	for i := range line {
+		digit := getNumber(line, i, accept_word)
+		if digit != ' ' {
+			digit1 = digit
 			break
 		}
 	}
 	for i := len(line) - 1; i >= 0; i-- {
-		if unicode.IsDigit(rune(line[i])) {
-			digit2 = rune(line[i])
+		digit := getNumber(line, i, accept_word)
+		if digit != ' ' {
+			digit2 = digit
 			break
 		}
 	}
@@ -39,7 +60,16 @@ func solveA(input string) int {
 	sum := 0
 	lines := strings.Split(input, "\n")
 	for _, line := range lines {
-		sum += findDigits(line)
+		sum += findDigits(line, false)
+	}
+	return sum
+}
+
+func solveB(input string) int {
+	sum := 0
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		sum += findDigits(line, true)
 	}
 	return sum
 }
@@ -49,6 +79,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	result := solveA(string(input))
-	fmt.Println(result)
+	resultA := solveA(string(input))
+	fmt.Printf("Result A: %v\n", resultA)
+
+	resultB := solveB(string(input))
+	fmt.Printf("Result B: %v\n", resultB)
 }
--- go/2023/day01/main_test.go
+++ go/2023/day01/main_test.go
@@ -1,9 +1,53 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
+
+type getNumberTest struct {
+	input    string
+	i        int
+	expected rune
+}
+
+var getNumberTests = []getNumberTest{
+	{"2", 0, '2'},
+	{"a5", 1, '5'},
+	{"a5", 0, ' '},
+	{"one", 0, '1'},
+	{"two", 0, '2'},
+	{"three", 0, '3'},
+	{"four", 0, '4'},
+	{"totwo", 2, '2'},
+}
+
+func TestGetNumber(t *testing.T) {
+	for _, test := range getNumberTests {
+		output := getNumber(test.input, test.i, true)
+		if output != test.expected {
+			t.Errorf("Got: %q, wanted: %q, for inputs (%q, %v)", output, test.expected, test.input, test.i)
+		}
+	}
+}
+
+func TestSolveB(t *testing.T) {
+	input := `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
+	got := solveB(input)
+	want := 281
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
 
 func TestFindDigits(t *testing.T) {
-	got := findDigits("a2bcd3ds4c")
+	got := findDigits("a2bcd3ds4c", false)
 	want := 24
 
 	if got != want {
