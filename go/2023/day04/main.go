28de3c29268e772e626218ed9141d528d21b09ba
Wolfram Friele <wolframfriele@gmail.com>
2023-12-04 20:37:11 +0100

    refactor 2023/day04 part A

--- go/2023/day04/main.go
+++ go/2023/day04/main.go
@@ -3,33 +3,58 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-type Game struct {
-	id      int
-	winning []int
-	have    []int
+type Card struct {
+	id     int
+	first  []int
+	second []int
 }
 
-//
-// func parseGame(line string) (game Game) {
-// 	splitResult := strings.Split(line[5:], ": ")
-// 	gameIndStr, allSelectionsStr := splitResult[0], splitResult[1]
-// 	gameInd, err := strconv.Atoi(gameIndStr)
-// 	if err != nil {
-// 		fmt.Printf("Coult not convert %v to integer", gameIndStr)
-// 	}
-// 	allSelectionsSlice := strings.Split(allSelectionsStr, "; ")
-// 	game.id = gameInd
-// 	for _, selectionStr := range allSelectionsSlice {
-//         if slices.Contains
-// 		game.selections = append(game.selections, parseSelection(selectionStr))
-// 	}
-// 	return
-// }
+func parseInput(input string) map[int]Card {
+	cards := make(map[int]Card)
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		if len(line) > 0 {
+			card := parseCard(line)
+			cards[card.id] = card
+		}
+	}
+	return cards
+}
 
-func Contains(checkIn []string, checkFor string) bool {
+func convertToIntSlice(stringSlice string) (intSlice []int) {
+	split := strings.Fields(stringSlice)
+	for _, numStr := range split {
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			fmt.Printf("Coult not convert %v to integer", numStr)
+		}
+		intSlice = append(intSlice, num)
+	}
+	return
+}
+
+func parseCard(line string) (card Card) {
+	splitResult := strings.Split(line[5:], ": ")
+	gameIndStr, cardNumbers := splitResult[0], splitResult[1]
+	gameInd, err := strconv.Atoi(gameIndStr)
+	if err != nil {
+		fmt.Printf("Coult not convert %v to integer", gameIndStr)
+	}
+	card.id = gameInd
+	splitNumbers := strings.Split(cardNumbers, " | ")
+	first, second := splitNumbers[0], splitNumbers[1]
+
+	card.first = convertToIntSlice(first)
+	card.second = convertToIntSlice(second)
+
+	return
+}
+
+func Contains(checkIn []int, checkFor int) bool {
 	for _, str := range checkIn {
 		if str == checkFor {
 			return true
@@ -38,19 +63,11 @@ func Contains(checkIn []string, checkFor string) bool {
 	return false
 }
 
-func solveA(input string) (sum int) {
-	lines := strings.Split(input, "\n")
-	for _, line := range lines {
-		if line == "" {
-			break
-		}
-		split01 := strings.Split(line[8:], " | ")
-		winning, have := split01[0], split01[1]
-		winningSplit := strings.Fields(winning)
-		haveSplit := strings.Fields(have)
+func solveA(cards map[int]Card) (sum int) {
+	for _, card := range cards {
 		gamePoints := 0
-		for _, code := range haveSplit {
-			if Contains(winningSplit, code) {
+		for _, code := range card.second {
+			if Contains(card.first, code) {
 				if gamePoints == 0 {
 					gamePoints = 1
 				} else {
@@ -64,29 +81,15 @@ func solveA(input string) (sum int) {
 }
 
 //	func solveB(games []Game) (sum int) {
-//		for _, game := range games {
-//			maxSize := Selection{0, 0, 0}
-//			for _, selection := range game.selections {
-//				if selection.green > maxSize.green {
-//					maxSize.green = selection.green
-//				}
-//				if selection.red > maxSize.red {
-//					maxSize.red = selection.red
-//				}
-//				if selection.blue > maxSize.blue {
-//					maxSize.blue = selection.blue
-//				}
-//			}
-//			sum += maxSize.red * maxSize.blue * maxSize.green
-//		}
-//		return
+//		//		return
 //	}
 func main() {
 	input, err := os.ReadFile("2023/day04/input.txt")
 	if err != nil {
 		panic(err)
 	}
-	resultA := solveA(string(input))
+	games := parseInput(string(input))
+	resultA := solveA(games)
 	fmt.Printf("Result A: %v\n", resultA)
 
 	// resultB := solveB(games)
--- go/2023/day04/main_test.go
+++ go/2023/day04/main_test.go
@@ -4,6 +4,30 @@ import (
 	"testing"
 )
 
+func TestParseCard(t *testing.T) {
+	input := "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+	got := parseCard(input)
+	want := Card{
+		id:     1,
+		first:  []int{41, 48, 83, 86, 17},
+		second: []int{83, 86, 6, 31, 17, 9, 48, 53},
+	}
+
+	if got.id != want.id {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+	for i, num := range got.first {
+		if num != want.first[i] {
+			t.Errorf("got %v, wanted %v", got, want)
+		}
+	}
+	for i, num := range got.second {
+		if num != want.second[i] {
+			t.Errorf("got %v, wanted %v", got, want)
+		}
+	}
+}
+
 func TestSolveA(t *testing.T) {
 	input := `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -11,7 +35,8 @@ Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
-	got := solveA(input)
+	games := parseInput(string(input))
+	got := solveA(games)
 	want := 13
 
 	if got != want {
